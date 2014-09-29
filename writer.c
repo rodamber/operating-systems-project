e854805d4930548db1fa9596b517309fc236853d
Rodrigo Bernardo <rodrigoambernardo@gmail.com>
2014-09-29 03:01:41 +0100

    Summary: Minor edits to writer.c
    Overview: Works fine on sigma except for the null characters. On mac, every file
    starts with its corresponding number. I don't get why.

--- writer.c
+++ writer.c
@@ -1,17 +1,15 @@
 /* writer.c */
 
-#include <stdio.h>
-
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include "wrrd.h"
 
 
-#define TIMES_TO_CYCLE 5120	/* number of times the whole process on writer.c
-				   will be done */
-#define TIMES_TO_WRITE 1024	/* number of strings that writer.c will write */
+#define TIMES_TO_CYCLE 5120	/* number of times the whole process on 
+				   writer.c will be done */
+#define TIMES_TO_WRITE 1024	/* number of strings that writer.c will
+				   write*/
 #define STRLEN 10		/* length of the strings to be written */
 
 
@@ -28,27 +26,19 @@ int main(void) {
     while(i--) {
 
 	int j = TIMES_TO_WRITE;
-	int oflags = O_WRONLY || O_CREAT;
-	int omodes = S_IRUSR || S_IWUSR || S_IROTH;
-	int fd = open("hello.txt", oflags , omodes);
-
-	printf("after open\n");
+	int oflags = O_WRONLY | O_CREAT;
+	int omodes = S_IRUSR | S_IWUSR | S_IROTH;
+	int fd = open(getfile(), oflags , omodes);
 
 	if (fd < 0)
 	    return 1;
 
-	printf("after open verification\n");
-
 	while(j--)
-	    if (write(fd, "hello\n", STRLEN) != STRLEN)
+	    if (write(fd, getstr(), STRLEN + 1) != STRLEN + 1)
 		return 1;
-
-	printf("after write\n");
 
 	if (close(fd) < 0)
 	    return 1;
-
-	printf("after close\n");
     }
     return 0;
 }
@@ -57,23 +47,28 @@ int main(void) {
 char *getfile() {
     int r = rand() % 5;
     char *file = (char*)malloc(sizeof(char) * 13);
-    char *filename = "SO2014-";
-    char letter  = 'a' + r;
+    char *prefix = "SO2014-";
+    char *number  = (char*)malloc(sizeof(char) * 2);
     char *ext = ".txt";
 
-    strcpy(file, filename);
-    strcat(file, &letter);
+    number[0] = '0' + r;
+    number[1] = '\0';
+
+    strcpy(file, prefix);
+    strcat(file, number);
     strcat(file, ext);
+
+    free(number);
 
     return file;
 }
 
 char *getstr() {
-    int r = rand() % 10, i = 0;
-    char *str = (char*)malloc(sizeof(char) * 12);
+    int  r = rand() % STRLEN, i = 0;
+    char *str = (char*)malloc(sizeof(char) * (STRLEN + 1));
     char letter = 'a' + r;
 
-    while(i++ < 10)
+    while(i++ < STRLEN - 1)
 	str[i] = letter;
 
     str[i++] = '\n';
