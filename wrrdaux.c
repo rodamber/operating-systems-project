4d2ffea22f01c30b4cd3de663823540a5d1519b8
Rodrigo Bernardo <rodrigoambernardo@gmail.com>
2014-09-29 16:16:22 +0100

    Overview: Added exec files wr and rd to .gitignore. 	Added new level of randomness to wrrdaux.c : now wr gives different files every time it executes, not every time it compiles.

--- wrrdaux.c
+++ wrrdaux.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "wrrd.h"
 
 void getfilename(char *dst) {
@@ -7,10 +8,16 @@ void getfilename(char *dst) {
      * Returns a string of type "SO2014-X.txt", where X is a random integer
      * between 0 and FILENUM.
      */
-    int r = rand() % FILENUM;
+    int r;
     char *prefix = "SO2014-";
-    char number[2] = {'0' + r};	/* number between 0 and FILENUM - 1 */
+    char number[2];
     char *ext = ".txt";
+
+    srand(time(NULL));
+    r = rand() % FILENUM;
+
+    number[0] = '0' + r;	/* number between 0 and FILENUM - 1 */
+    number[1] = '\0';
 
     strcpy(dst, prefix);
     strcat(dst, number);
@@ -23,8 +30,13 @@ void getstr(char *dst) {
      * Returns a STRLEN size character string composed of STRLEN - 1 equal 
      * characters between 'a' and 'a' + STRLEN plus a '\n'.
      */
-    int  r = rand() % STRLEN, i = 0;
-    char letter = 'a' + r;
+    int  r, i = 0;
+    char letter;
+
+    srand(time(NULL));
+    r = rand() % STRLEN;
+
+    letter = 'a' + r;
 
     while(i < STRLEN - 1)
 	dst[i++] = letter;
