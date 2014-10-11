48a0b56ae855257fcd0e0b2d2a4ee4e2d44bc541
Rodrigo Bernardo <rodrigoambernardo@gmail.com>
2014-10-11 22:10:25 +0100

    Forgot to commit src (don't judge).

---
+++ src/Ex1/wrrd.h
@@ -0,0 +1,35 @@
+/*
+ * Identification
+ *
+ * Group 24
+ *
+ * 78294  José Semedo
+ * 78471  João Marçal
+ * 78942  Rodrigo Bernardo
+ */
+
+#ifndef __WRRD_H__
+#define __WRRD_H__
+
+#define CYCLENUM 5120           /* number of times to do the whole process */
+#define STRNUM   1024	        /* number of strings to write */
+#define STRLEN   10		/* length of the strings to be written */
+#define FILENUM  5		/* number of possible files to write on */
+#define FNLEN    12		/* filename length */
+
+
+/*
+ * dst will point to a string like "SO2014-X.txt", where X is a random integer
+ * between 0 and FILENUM. Uses the rand() function, so maybe you'll want to
+ * set a seed using srand().
+ */
+void getfile(char *dst);
+
+/*
+ * dst will point to a STRLEN sized string composed of STRLEN - 1  equal
+ * characters between 'a' and 'a' + STRLEN plus a '\n'. Uses the rand()
+ * function, so maybe you'll want to set a seed using srand().
+ */
+void getstr(char *dst);
+
+#endif
---
+++ src/Ex1/wrrdaux.c
@@ -0,0 +1,45 @@
+/*
+ * Identification
+ *
+ * Group  24
+ *
+ * 78294  José Semedo
+ * 78471  João Marçal
+ * 78942  Rodrigo Bernardo
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "wrrd.h"
+
+
+void getfile(char *dst) {
+    int r;
+    char *prefix = "SO2014-";
+    char number[2];
+    char *ext = ".txt";
+
+    r = rand() % FILENUM;
+
+    number[0] = '0' + r;
+    number[1] = '\0';
+
+    strcpy(dst, prefix);
+    strcat(dst, number);
+    strcat(dst, ext);
+}
+
+void getstr(char *dst) {
+    int  r;
+    char letter;
+
+    r = rand() % STRLEN;
+
+    letter = 'a' + r;
+
+    for (r = 0; r < STRLEN - 1; r++)
+	dst[r] = letter;
+
+    dst[r++] = '\n';
+    dst[r] = '\0';
+}
