def7ad524f4a7fab36c3e6bfbc79e42a10386bcc
Rodrigo Bernardo <rodrigoambernardo@gmail.com>
2014-10-20 21:41:12 +0100

    Parallel writer now works. Time displayed should be on microseconds. Makefile calls Makefile from ../Ex1 and p_wr calls wr from ../Ex1.

--- src/Ex2/parallel_writer.c
+++ src/Ex2/parallel_writer.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <time.h>
@@ -10,27 +11,32 @@
 int main(void) {
 
     int i;
-    int nb_forks = 0;		/* number of child processes created */
+    int nb_forks = 0;		/* stores number of child processes created */
     int status = 0;
-    time_t t = time(NULL);
+    time_t start = time(NULL);
 
+    printf("Creating child processes...\n");
     for(i = 0; i < NB_CHILDS; i++) {
 	pid_t pID = fork();
 
 	if (pID == 0) {
 	    nb_forks++;
-	    execl("wr", NULL);
+	    execl("../Ex1/wr", NULL);
+            exit(0);
+	    /* chamar exit ou nao chamar exit, eis a questao... */
 	}
 	else if (pID < 0) {
 	    printf("Failed to fork!\n");
 	}
     }
 
-    for(i = 0; i < nb_forks; i++) {
+    printf("Waiting for childs to finish...\n");
+    for(i = 0; i < NB_CHILDS; i++) {
 	wait(&status);
+	/* verificar o status? */
     }
 
-    printf("Time: %d\n", (int)(time(NULL) - t));
-
+    printf("Finished. Time: ~%ds\n", (int)(time(NULL) - start));
+    /* a ideia e por isto a dar com precisao de milisegundos. nao tentem fazer cast para double ou float, nao vai funcionar */
     return 0;
 }
