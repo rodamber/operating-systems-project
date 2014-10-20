ee58abdad25b6e02b2a66cf705ee9f0830c47e04
Rodrigo Bernardo <rodrigoambernardo@gmail.com>
2014-10-20 21:57:03 +0100

    Changed 5120 iterations to 512.

--- src/Ex1/wrrd.h
+++ src/Ex1/wrrd.h
@@ -11,7 +11,7 @@
 #ifndef __WRRD_H__
 #define __WRRD_H__
 
-#define CYCLENUM 5120           /* number of times to do the whole process */
+#define CYCLENUM 512           /* number of times to do the whole process */
 #define STRNUM   1024	        /* number of strings to write */
 #define STRLEN   10		/* length of the strings to be written */
 #define FILENUM  5		/* number of possible files to write on */
