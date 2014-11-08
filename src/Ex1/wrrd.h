f37cb984e100a95944110b4ae50c1558caff556f
Rodrigo Bernardo <rodrigoambernardo@gmail.com>
2014-11-08 12:31:28 +0000

    Actualizei a documentação do wrrd.h

--- src/Ex1/wrrd.h
+++ src/Ex1/wrrd.h
@@ -1,35 +1,26 @@
-/*
- * Identification
- *
- * Group 24
- *
- * 78294  José Semedo
- * 78471  João Marçal
- * 78942  Rodrigo Bernardo
- */
-
 #ifndef __WRRD_H__
 #define __WRRD_H__
 
-#define CYCLENUM 512           /* number of times to do the whole process */
+#define CYCLENUM 512           /* number of iterations */
 #define STRNUM   1024	        /* number of strings to write */
 #define STRLEN   10		/* length of the strings to be written */
 #define FILENUM  5		/* number of possible files to write on */
-#define FNLEN    12		/* filename length */
+#define FNLEN    12		/* file name length */
 
 
 /*
- * dst will point to a string like "SO2014-X.txt", where X is a random integer
- * between 0 and FILENUM. Uses the rand() function, so maybe you'll want to
- * set a seed using srand().
- * If n >= 0, then X becomes equal to n; otherwise X is random.
+ * dst will point to a string like "SO2014-X.txt", where X is n if n >= 0.
+ * Otherwise X will be a random integer between 0 and FILENUM - 1.
+ *
+ * Uses rand(), so maybe you'll want to set a seed using srand().
  */
 void getfile(char *dst, char n);
 
 /*
  * dst will point to a STRLEN sized string composed of STRLEN - 1  equal
- * characters between 'a' and 'a' + STRLEN plus a '\n'. Uses the rand()
- * function, so maybe you'll want to set a seed using srand().
+ * characters between 'a' and 'a' + STRLEN followed by a '\n'.
+ *
+ * Uses rand(), so maybe you'll want to set a seed using srand().
  */
 void getstr(char *dst);
 
