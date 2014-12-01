aadc696d641a7d69fd8cfd22abc7c4bd2eaafe15
Rodrigo Bernardo <rodrigoambernardo@gmail.com>
2014-12-01 04:55:10 +0000

    new file:   parallel_reader1.c

---
+++ src/Ex5/1/rodrigo/parallel_reader1.c
@@ -0,0 +1,94 @@
+/*perguntar ao professor se e preciso fazer verificacoes para inicializaçao (por exemplo, sem_init), e para utilizaçao (por exemplo, sem_wait, sem_post) dos objectos de sincronizaçao*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#include "../../../Ex1/wrrd.h"
+
+#define BUFFER_SIZE 10
+#define NB_THREADS  50
+#define FINISH      "sair"
+
+char buffer[BUFFER_SIZE][FNLEN + 1];
+int  nb_readers = 0;
+int  next_read_index = 0;
+int  next_write_index = 0;
+
+sem_t sem_info;
+sem_t sem_no_info;
+pthread_mutex_t buffer_mutex;
+
+
+int main(void) {
+    char input;
+    char filename[FNLEN + 1];
+    pthread_t readers_ids[NB_THREADS];
+
+    /**
+     * Sincronization objects initialization.
+     */
+    if (pthread_mutex_init(&buffer_mutex, NULL) || sem_init(&sem_info,0, 0) || sem_init(&sem_no_info, 0, BUFFER_SIZE)) {
+        perror("Error creating sincronization objects");
+        exit(-1);
+    }
+
+    /**
+     * Thread launch.
+     */
+    for (i = 0; i < NB_THREADS; i++) {
+        if (pthread_create(&readers_ids[i], NULL, &reader, NULL)) {
+            perror("Error creating thread");
+            exit(-1);
+        }
+    }
+
+    /**
+     * Read filenames from input. Finish when "sair" is read from input.
+     */
+    while (!strcmp(filename, FINISH)) {
+        if (read(0, &input, 1) < 0) {
+            perror("Error reading from stdin");
+            exit(-1);
+        }
+
+        if (!strcmp(input, ' ')) {
+            continue;
+        }
+
+        filename = strncat(filename, &input, 1);
+
+        sem_wait(&sem_info);
+        pthread_mutex_lock(&buffer_mutex);
+
+        buffer = strcpy(buffer[next_write_index], filename);
+        (next_write_index++) % BUFFER_SIZE;
+        filename[0] = '\0';
+
+        pthread_mutex_unlock(&buffer_mutex);
+        sem_post(&sem_info);
+    }
+
+    /**
+     * Join threads
+     */
+    for (i = 0; i < NB_THREADS; i++) {
+        int* return_value_ptr = NULL;
+        if (pthread_join(readers_ids[i], (void**) &return_value_ptr)) {
+            perror("Error joining threads");
+            exit(-1);
+        }
+        printf("Thread %d/%d returned %d\n", i, NB_THREADS, *return_value_ptr);
+    }
+
+    /**
+     * Sincronization objects elimination.
+     */
+    if (pthread_mutex_destroy(&buffer_mutex) || sem_destroy(&sem_info) || sem_destroy(&sem_no_info)) {
+        perror("Error destroying sincronization objects");
+        exit(-1);
+    }
+
+    exit(0);
+}
