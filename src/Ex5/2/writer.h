81344ede5fbe9b764c51a10f329ae78fa1861e5c
Rodrigo Bernardo <rodrigoambernardo@gmail.com>
2014-12-02 23:33:00 +0000

    new file:   writer.h

---
+++ src/Ex5/2/writer.h
@@ -0,0 +1,13 @@
+#ifndef __WRITER_H__
+#define __WRITER_H__
+
+
+#define NB_WRITERS  50
+
+int get_file_locking (void);
+int get_error_writing(void);
+int get_finish_flag  (void);
+
+void* writer(void*);
+
+#endif /* end of include guard: __WRITER_H__ */
