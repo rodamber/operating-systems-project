8e65f354217cf3a94c530bc313fc06a20e0da734
Rodrigo Andre Moreira Bernardo <ist178942@sigma03.ist.utl.pt>
2014-12-02 23:39:19 +0000

    modified:   ../1/reader.c modified:   ../1/reader.h modified:   Makefile modified:   parallel_writer.c

--- src/Ex5/1/reader.h
+++ src/Ex5/1/reader.h
@@ -1,21 +1,24 @@
 #ifndef __READER_H__
 #define __READER_H__
 
+
 #include "../../Ex1/wrrd.h"
 
 #define BUFFER_SIZE 10
 #define NB_READERS  20
 
-extern char buffer[BUFFER_SIZE][FNLEN + 1];
+char buffer[BUFFER_SIZE][FNLEN + 1];
+
 extern int  next_read_index;
 extern int  next_write_index;
 
-extern sem_t sem_no_info;
-extern sem_t sem_info;
-extern pthread_mutex_t buffer_mutex;
+sem_t 		   sem_no_info;
+sem_t 		   sem_info;
+pthread_mutex_t buffer_mutex;
 
 
 void* reader(void*);
 void* writer(void*);
+
 
 #endif /* end of include guard: __READER_H__ */
