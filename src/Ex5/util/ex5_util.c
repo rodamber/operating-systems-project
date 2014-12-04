6b0f9d57a19fa7135b8e07eae7bbbee66132aeb7
Rodrigo Andre Moreira Bernardo <ist178942@nexus1.rnl.ist.utl.pt>
2014-12-04 21:26:38 +0000

    Eliminei coisas a mais no util

--- src/Ex5/util/ex5_util.c
+++ src/Ex5/util/ex5_util.c
@@ -6,21 +6,6 @@
 #include "ex5_util.h"
 
 
-int main(void) {
-    char word[13] = {'\0'};
-    int ret;
-
-    printf("Hello mate. Insert a word\n");
-    while (strcmp(word, "sair") != 0) {
-        if ((ret = read_word(word, 12)) == -1) {
-            die("Error reading from stdin");
-        }
-        printf("That's your word mate: %s\n", word);
-    }
-    printf("return of read_word: %d\n", ret);
-    return EXIT_SUCCESS;
-}
-
 void die(const char* msg) {
     perror(msg);
     exit(-1);
@@ -50,33 +35,5 @@ char read_char() {
         return -1;
     }
     return c;
-}
-
-/**
- * Reads a word from stdin.
- */
-int read_word2(char* dst, int max_length) {
-    int  i;
-    char c;
-
-    for (i = 0; i < max_length; i++) {
-        if (read(STDIN_FILENO, &c, 1) == -1) {
-            return -1;
-        }
-        if (c != '\n' && c != ' ' && c != EOF) {
-            dst[i] = c;
-        }
-        else {
-            break;
-        }
-    }
-    dst[i] = '\0';
-
-    while (c != '\n' && c != ' ' && c != EOF) {
-        if (read(STDIN_FILENO, &c, 1) == -1) {
-            return -1;
-        }
-    }
-    return i;
 }
 
